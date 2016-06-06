0a46f7da893e69e685223f78eef729bdad7deedf
bcchu <96benchu@gmail.com>
2016-06-06 13:55:06 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+var usernames = {};
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+io.on('connection', function(socket){
+  //add to list
+  socket.on('name', function(userid){
+    usernames[socket.id] = userid;
+  });  
+
+  //send message
+  socket.on('chat message', function(msg){
+    io.emit('chat message', {"user" :usernames[socket.id], "message": msg});
+  });
+  
+  //delete from list
+  socket.on("disconnect", function(){
+    delete usernames[socket.id];
+    });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
